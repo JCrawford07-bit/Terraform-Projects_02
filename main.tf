debf6ed2d90a5d7ddcd196cd5354258e153fffe4
JCrawford07-bit <jennycrawford.motion@gmail.com>
2024-01-17 16:54:42 +0000

    destroyed and made a new file

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 resource "aws_instance" "jennyexample0706" {
   ami           =  "ami-0fb653ca2d3203ac1" 
   instance_type = "t2.micro"
- tags = {
-    Name = "jenny-instance-example-01"
-  }
 }
+ 
+ 
